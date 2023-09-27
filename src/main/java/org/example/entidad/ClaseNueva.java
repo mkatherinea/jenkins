15c702c1da0596d9fda143351cd28d00da113849
mkatherinea <87kalvarez@gmail.com>
2023-09-27 17:51:10 -0300

    A commit has been made.04

--- src/main/java/org/example/entidad/ClaseNueva.java
+++ src/main/java/org/example/entidad/ClaseNueva.java
@@ -2,4 +2,23 @@ package org.example.entidad;
 
 public class ClaseNueva {
     String nombre;
+
+    public ClaseNueva(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "ClaseNueva{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
 }
