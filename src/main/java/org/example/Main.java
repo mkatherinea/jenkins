c1b8a18155733700f3a8508f8989ea840168e12d
mkatherinea <87kalvarez@gmail.com>
2023-09-27 19:59:58 -0300

    otra cosa 02

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("A commit has been made zaraza.");
+
 
         System.out.println("Esto es pa ver cambios :)");
         System.out.println("Menem esta vivo?");
